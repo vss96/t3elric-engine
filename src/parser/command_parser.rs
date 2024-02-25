a36077f6e22d6941cadc311a31339295cdf148fd
Vikas S Shetty <shettyvikas209@gmail.com>
2024-02-25 23:07:15 +0000

    Refactor CommandParser to build up the Individual commands

--- src/parser/command_parser.rs
+++ src/parser/command_parser.rs
@@ -1,83 +1,94 @@
 use either::Either;
 
 use super::{
-    identify_parser::{Identify, IdentifyParser, IDENTIFY},
-    move_parser::{BoardState, MoveParser, MOVE},
-    parse_result::ParseResult,
-    parser::Parser,
-    quit_parser::{QuitParser, QUIT},
-    step_parser::{Step, StepParser, ST3P},
+    identify_parser::{Identify, IdentifyParser},
+    move_parser::{map_to_move, BoardState, MoveParser, MoveParserReturnType},
+    or_parser::Or4,
+    quit_parser::{Quit, QuitParser},
+    step_parser::{Step, StepParser, StepParserReturnType},
     time_parser::TimeSetting,
     version_parser::Version,
 };
 
+#[derive(Eq, PartialEq, Debug)]
 pub enum Command {
-    Step,
-    Identify,
-    Move,
-    Quit,
+    Step(Step),
+    Identify(Identify),
+    Move(BoardState),
+    Quit(Quit),
 }
 
-pub struct CommandParser;
+pub type CommandParser = Or4<StepParser, IdentifyParser, MoveParser, QuitParser>;
+pub type ComandParserReturnType =
+    Either<StepParserReturnType, Either<String, Either<MoveParserReturnType, String>>>;
 
-impl Parser<Command> for CommandParser {
-    fn parse_from(val: &String) -> ParseResult<Command> {
-        match val {
-            s if s.starts_with(ST3P) => Ok((Command::Step, val.to_string())),
-            s if s.starts_with(IDENTIFY) => Ok((Command::Identify, val.to_string())),
-            s if s.starts_with(MOVE) => Ok((Command::Move, val.to_string())),
-            s if s.starts_with(QUIT) => Ok((Command::Quit, val.to_string())),
-            _ => Err(String::from("Invalid Command")),
+pub fn map_to_command(parser_output: ComandParserReturnType) -> Command {
+    return match parser_output {
+        Either::Left(output) => {
+            let (_, (_, (_, (_, version)))) = output;
+
+            Command::Step(Step(Version(version)))
         }
-    }
+        Either::Right(Either::Left(_)) => Command::Identify(Identify::new()),
+        Either::Right(Either::Right(Either::Left(output))) => Command::Move(map_to_move(output)),
+        Either::Right(Either::Right(Either::Right(_))) => Command::Quit(Quit {}),
+    };
 }
 
 impl Command {
-    pub fn execute(&self, val: &String) -> Result<(), String> {
-        match *self {
-            Command::Step => {
-                let res = StepParser::parse_from(val)?;
-                let ((_, (_, (_, (_, version)))), _) = res;
-
-                println!("{}", Step(Version(version)));
+    pub fn execute(&self) -> Result<(), String> {
+        match self {
+            Command::Step(step) => {
+                println!("{}", step);
             }
-            Command::Identify => {
-                let _ = IdentifyParser::parse_from(val)?;
-                println!("{}", Identify::new());
+            Command::Identify(identify) => {
+                println!("{}", identify);
             }
-            Command::Move => {
-                let res = MoveParser::parse_from(val)?;
-
-                let move_command: BoardState = match res.0 {
-                    Either::Left((((_, (_, board)), player), (_, (_, (_, time))))) => BoardState {
-                        player_to_move: player,
-                        board,
-                        time_setting: TimeSetting::TotalTime(time),
-                    },
-                    Either::Right(Either::Left((
-                        ((_, (_, board)), player),
-                        (_, (_, (_, time))),
-                    ))) => BoardState {
-                        player_to_move: player,
-                        board,
-                        time_setting: TimeSetting::TimeRemaining(time),
-                    },
-
-                    Either::Right(Either::Right((((_, (_, board)), player), _))) => BoardState {
-                        player_to_move: player,
-                        board,
-                        time_setting: TimeSetting::Infinite,
-                    },
-                };
-
-                println!("{}", move_command.best_move());
+            Command::Move(board_state) => {
+                // println!("{}", board_state.best_move());
             }
-            Command::Quit => {
-                let res = QuitParser::parse_from(val)?;
-                res.0.exit_engine();
+            Command::Quit(quit) => {
+                quit.exit_engine();
             }
         };
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test_command {
+    use crate::parser::{
+        command_parser::Command,
+        move_parser::{
+            Board, BoardState,
+            Cell::{Playable, Played},
+            Player,
+        },
+        time_parser::TimeSetting,
+        Parser,
+    };
+
+    use super::{map_to_command, CommandParser};
+
+    #[test]
+    fn test_move() {
+        let move_string = String::from("move 3_/_x_/3_ o");
+        let val = &move_string.clone();
+        let command = CommandParser::parse_from(val).map(|(res, _)| map_to_command(res));
+        assert_eq!(
+            command,
+            Ok(Command::Move(BoardState {
+                player_to_move: Player::O,
+                board: Board {
+                    rows: vec![
+                        vec![Playable, Playable, Playable],
+                        vec![Playable, Played(Player::X), Playable],
+                        vec![Playable, Playable, Playable]
+                    ]
+                },
+                time_setting: TimeSetting::Infinite
+            }))
+        );
     }
 }
