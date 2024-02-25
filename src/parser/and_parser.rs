493561d8b62f82f17f56c3a43af4ec16489db5bb
Vikas S Shetty <shettyvikas209@gmail.com>
2024-02-25 22:59:01 +0000

    Add variants to AndParser - AndN

--- src/parser/and_parser.rs
+++ src/parser/and_parser.rs
@@ -1,20 +1,20 @@
 use super::{ParseResult, Parser};
 
-pub struct AndParser<P1, P2> (P1, P2);
+pub struct AndParser<P1, P2>(P1, P2);
 
-impl<P1, P2, X, Y> Parser<(X, Y)> for AndParser<P1, P2>
+impl<P1, P2, X, Y> Parser<(X, Y)> for And2<P1, P2>
 where
     P1: Parser<X>,
     P2: Parser<Y>,
 {
     fn parse_from(val: &String) -> ParseResult<(X, Y)> {
-        P1::parse_from(val)
-        .and_then(|(x, follow)| {
-            P2::parse_from(&follow)
-            .map(|(y, remaining)| ((x, y), remaining))
+        P1::parse_from(val).and_then(|(x, follow)| {
+            P2::parse_from(&follow).map(|(y, remaining)| ((x, y), remaining))
         })
     }
 }
 
-
-pub type TriAnd<U, V, W > = AndParser< U, AndParser<V, W> >;
+pub type And2<U, V> = AndParser<U, V>;
+pub type And3<U, V, W> = And2<U, And2<V, W>>;
+pub type And4<U, V, W, X> = And2<U, And3<V, W, X>>;
+pub type And5<U, V, W, X, Y> = And2<U, And4<V, W, X, Y>>;
