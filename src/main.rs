b8bb72c6bc90680ce0e559809cc4c10232b4eaa4
Vikas S Shetty <shettyvikas209@gmail.com>
2024-02-27 21:21:58 +0000

    Add a main fn loop to read input and parse command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::io::{stdin, Result};
+
 mod parser;
-fn main() {
-    println!("Hello, world!");
+mod solver;
+use parser::{map_to_command, CommandParser, Parser};
+
+fn main() -> Result<()> {
+    loop {
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer)?;
+        let input_string = buffer.trim().to_owned();
+        let command =
+            CommandParser::parse_from(&input_string).map(|(output, _)| map_to_command(output));
+
+        let _ = command.unwrap().execute();
+    }
 }
